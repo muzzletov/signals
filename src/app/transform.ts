fc50e0c6b3b8e882901ff5e1a6f8fb1e8684f40c
muzzletov <m.velimirov@gmail.com>
2025-04-20 19:39:20 +0200

    Update transform.ts

--- src/app/transform.ts
+++ src/app/transform.ts
@@ -1,50 +1,82 @@
-import {signal, Signal} from "@angular/core";
+import { signal, WritableSignal } from '@angular/core';
 
-type Extended<T extends Record<string, any>> = {
-  [K in keyof T]: T[K] extends Record<string, any>
-    ? Extended<T[K]>
-    : T[K];
+export type Reactive<T extends Record<string, any>> = {
+  [K in keyof T]: T[K] extends Record<string, any> ? Reactive<T[K]> : T[K];
 } & {
-  [K in keyof T as `_${K & string}`]: Signal<T[K] extends Record<string, any>
-    ? Extended<T[K]>
-    : T[K]>;
+  [K in keyof T as `$${K & string}`]: WritableSignal<
+    T[K] extends Record<string, any> ? Reactive<T[K]> : T[K]
+  >;
 };
 
-export function transform<T extends Record<string, any>>(obj: T): Extended<T> {
-  const extendedObj: Partial<Extended<T>> = {};
-  const isArray = Array.isArray(obj)
+export function transform<T extends Record<string, any>>(obj: T): Reactive<T> {
+  if (obj === null || typeof obj !== 'object') return obj;
+
+  const extendedObj: Partial<Reactive<T>> = {};
+  const isArray = Array.isArray(obj);
 
   if (isArray) return obj;
 
   for (const key in obj) {
-    if (!obj[key]) continue;
+    const hasProperty = Object.prototype.hasOwnProperty.call(obj, key);
 
-    if (Object.prototype.hasOwnProperty.call(obj, key) && typeof obj[key] === "object") {
-      let currentValue = transform(obj[key]);
-
-      const valueSignal = signal<typeof currentValue>(currentValue);
-
-      Object.defineProperty(extendedObj, key, {
-        get() {
-          return currentValue;
-        },
-        set(newValue: typeof currentValue) {
-          valueSignal.set(currentValue);
-          currentValue = transform(newValue);
-        },
-        enumerable: true,
-        configurable: true,
-      });
-
-      Object.defineProperty(extendedObj, `_${key}` as keyof Extended<T>, {
-        get() {
-          return valueSignal;
-        },
-        enumerable: false,
-        configurable: false,
-      });
+    if (!hasProperty) {
+      continue;
     }
+
+    let currentValue = transform(obj[key]);
+
+    const valueSignal = signal(Symbol());
+
+    Object.defineProperty(extendedObj, key, {
+      get() {
+        return currentValue;
+      },
+      set(newValue: typeof currentValue) {
+        valueSignal.set(Symbol());
+        if (newValue !== currentValue) {
+          currentValue = transform(newValue);
+        }
+      },
+      enumerable: true,
+      configurable: true,
+    });
+
+    Object.defineProperty(extendedObj, `$${key}` as keyof Reactive<T>, {
+      get() {
+        return valueSignal;
+      },
+      enumerable: false,
+      configurable: false,
+    });
   }
 
-  return extendedObj as Extended<T>;
+  return extendedObj as Reactive<T>;
+}
+
+export function reactive<T extends Record<string, any>>(initial: T) {
+  const valueSignal = signal(Symbol());
+
+  let currentValue = transform(initial);
+
+  const obj = {
+    get value() {
+      return currentValue;
+    },
+    set value(newValue: typeof currentValue) {
+      valueSignal.set(Symbol());
+      if (newValue !== currentValue) {
+        currentValue = transform({ ...initial, ...(newValue ?? {}) });
+      }
+    },
+  };
+
+  Object.defineProperty(obj, '$value', {
+    get() {
+      return valueSignal;
+    },
+    enumerable: false,
+    configurable: false,
+  });
+
+  return obj as Reactive<{ value: T }>;
 }
